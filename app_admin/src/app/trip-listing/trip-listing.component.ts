b61554dfd7121550464b794ddc2617d06f1e9c06
Fabian Rodriguez <fabian.rodrez@gmail.com>
2023-10-07 22:16:17 -0500

    mod 6 angular

---
+++ app_admin/src/app/trip-listing/trip-listing.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from "@angular/core";
+import { trips } from "../data/trips";
+
+type Trip = {
+  code: string;
+  name: string;
+  length: string;
+  start: string;
+  resort: string;
+  perPerson: string;
+  image: string;
+  description: string;
+};
+
+@Component({
+  selector: "app-trip-listing",
+  templateUrl: "./trip-listing.component.html",
+  styleUrls: ["./trip-listing.component.css"],
+})
+export class TripListingComponent implements OnInit {
+  trips: Array<Trip> = trips;
+
+  constructor() {}
+  ngOnInit(): void {}
+}
